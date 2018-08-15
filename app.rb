9a7b8d5ed3b4d798d40f1261d22e65ecc419f817
ananyagupta1114 <ananyagupta1114@gmail.com>
2018-08-15 14:18:58 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ class App < Sinatra::Base
   # Write your code here!
   get '/reversename/:name' do 
     @username = params[:name].reverse
-    "Hi #{@user_name}!"
+    "Hi #{@username}!"
   end
 
 end
